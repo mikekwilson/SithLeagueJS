fbf2dd6a9ed1b989e56e38c975258f15f3e55c8b
Mike Wilson <mikeismint@gmail.com>
2017-04-11 22:58:05 +0100

    Add gulp default task Add gulp html task to move html views from src to build

--- gulpfile.js
+++ gulpfile.js
@@ -23,9 +23,19 @@ gulp.task('clean', function() {
  * build directory
  */
 gulp.task('scripts', function() {
-  return gulp.src(paths.source + 'js/*.js')
-    .pipe(gulp.dest(paths.build + 'js'));
+  return gulp.src(paths.source + '**/*.js')
+    .pipe(gulp.dest(paths.build));
 });
+
+/**
+* HTML task
+*
+* Moves html files to build directory
+*/
+gulp.task('html', function() {
+  return gulp.src(paths.source + '**/*.html')
+    .pipe(gulp.dest(paths.build));
+})
 
 /**
 * Build task
@@ -33,8 +43,10 @@ gulp.task('scripts', function() {
 * Inserts script tag into index.html for each file in the project
 * ignores `app.js` which is loaded first.
 */
-gulp.task('build', ['scripts'],  function() {
-  var target = gulp.src(paths.source + 'index.html'),
+gulp.task('build', ['html', 'scripts'],  function() {
+  gulp.src(paths.source + 'index.html').pipe(gulp.dest(paths.build));
+
+  var target = gulp.src(paths.build + 'index.html'),
       source = gulp.src([
         paths.build + '**/*.js', 
         '!'+paths.build+'app.js'], 
@@ -53,7 +65,17 @@ gulp.task('build', ['scripts'],  function() {
 gulp.task('watch',['build'], function() {
   //watch js files
   gulp.watch(paths.source + '**/*.js', ['build']);
+ 
+  // watch index.html
+  gulp.watch(paths.source + 'index.html', ['build']);
 
   // watch html files
-  gulp.watch(paths.source + '**/*.html', ['build']);
+  gulp.watch(paths.source + '**/*.html', ['html']);
 });
+
+/**
+* Default task
+*
+* Builds app then watches for changes.
+*/
+gulp.task('default', ['build', 'watch'])
